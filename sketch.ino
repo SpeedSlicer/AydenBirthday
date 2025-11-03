9b6592c158697ccfced702cd88c2e07543680ff7
SpeedSlicer <143555953+SpeedSlicer@users.noreply.github.com>
2025-11-02 20:21:27 -0500

    Refactor LCD text display and add typeLine function
    Refactor text display to use typeLine function and improve readability.

--- sketch.ino
+++ sketch.ino
@@ -1,7 +1,7 @@
 #include <LiquidCrystal.h>
 
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
-
+// 50% OF DYNAMIC MEMORY :SOB:
 const char* speech[] = {
   "If you see text ",
   "on this screen,",
@@ -11,26 +11,34 @@ const char* speech[] = {
   "Ayden!         ",
   "You've been a  ",
   "critical part  ",
-  "of my life since",
-  " 6th grade.    ",
+  "of my life     ",
+  "since 6th grade",
   "Your impact is ",
   "incredibly HUGE",
   "on my life.    ",
   "You are a day 1",
   "friend and made",
-  "an impact to   ",
+  "an impact on   ",
   "everyone you met",
   ".              ",
-  "You still talked",
+  "You still spoke ",
   "to me when I   ",
   "wasn't as good ",
   "at socializing,",
   "and inspired me",
-  "to change myself",
-  "for the better.",
+  "to change      ",
+  "myself for the ",
+  "better.        ",
   "Thanks for     ",
   "always being   ",
   "there.         ",
+  "Anyways, since ",
+  "I'm not a poet ",
+  "and I'm running",
+  "out of memory, ",
+  "I guess my     ",
+  "message ends   ",
+  "here!          ",
   "- Shaun!       ",
   "               ",
   "If you want to ",
@@ -96,32 +104,30 @@ void setup() {
   lcd.createChar(2, cakeFlame2);
   lcd.createChar(3, cakeBase);
   lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Message Loading");
+  lcd.setCursor(0, 1);
+  lcd.print("...            ");
+  delay(5000);
 }
 
 void loop() {
   for (int i = 0; i < speechCount; i += 2) {
     lcd.clear();
-    lcd.setCursor(0, 0);
-    lcd.print(speech[i]);
+    typeLine(0, speech[i]);
     if (i + 1 < speechCount) {
-      lcd.setCursor(0, 1);
-      lcd.print(speech[i + 1]);
+      typeLine(1, speech[i + 1]);
     }
-    delay(2000);
+    delay(800);
   }
-
   lcd.clear();
-
   lcd.setCursor(0, 0);
   lcd.setCursor(14, 0);
   lcd.print(" ");
   lcd.setCursor(0, 0);
   lcd.print("HAPPY BIRTHDAY");
 
-
-
   while (true) {
-
     lcd.setCursor(0, 1);
     lcd.write(byte(0));
     lcd.setCursor(7, 1);
@@ -129,8 +135,6 @@ void loop() {
     lcd.setCursor(15, 1);
     lcd.write(byte(0));
     delay(500);
-
-
     lcd.setCursor(0, 1);
     lcd.write(byte(0));
     lcd.setCursor(7, 1);
@@ -138,5 +142,12 @@ void loop() {
     lcd.setCursor(15, 1);
     lcd.write(byte(0));
     delay(500);
+  }
+}
+void typeLine(int row, const char* text) {
+  lcd.setCursor(0, row);
+  for (int i = 0; i < 16 && text[i] != '\0'; i++) {
+    lcd.print(text[i]);
+    delay(100);
   }
 }
